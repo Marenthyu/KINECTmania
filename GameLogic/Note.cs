89e25ad67318057b228ac0850f97cb4f1e15f328
Marenthyu <marenthyu@marenthyu.de>
2017-05-10 14:05:13 +0200

    First FileParser and unfinished Song/Not implementation.

---
+++ GameLogic/Note.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KINECTmania.GameLogic
+{
+    class Note
+    {
+        private long startTime;
+        private short position;
+
+        public Note(long startTime, short position)
+        {
+            this.startTime = startTime;
+            this.position = position;
+        }
+
+        public static Note NoteFromLine(String line)
+        {
+            long startTime;
+            short position;
+            String[] parts = line.Split(' ');
+
+            bool startSuccess = long.TryParse(parts[0], out startTime);
+            if (!startSuccess)
+            {
+                throw new FormatException(String.Format("Could not parse Note, invalid long for startTime: {0}", parts[0]));
+            }
+            bool positionSuccess = short.TryParse(parts[1], out position);
+            if (!positionSuccess)
+            {
+                throw new FormatException(String.Format("Could not parse Note, invalid Int16 for position: {0}", parts[1]));
+            }
+            return new Note(startTime, position);
+        }
+
+        public long StartTime()
+        {
+            return this.startTime;
+        }
+
+        public short Position()
+        {
+            return this.position;
+        }
+    }
+}
